aa5378bb22af4e6a93be5f549e0fca7911a839d6
satyx <satyamcrj@gmail.com>
2018-01-15 01:19:55 +0530

    Implementing foreach loop in java

---
+++ foreach_loop.java
@@ -0,0 +1,12 @@
+class foreach_loop
+{
+	public static void main(String args[])
+	{
+		String arr[]={"Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday"};
+
+		for(String a:arr)
+		{
+			System.out.println(a);
+		}
+	}
+}
